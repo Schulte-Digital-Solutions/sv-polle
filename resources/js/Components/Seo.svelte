ed9423a7d6f99e50eab8c14272ff75a9223c77ae
Benedikt Schulte <benedikt@bullshitmedia.de>
2025-03-19 11:03:26 +0100

    Add SEO component and implement meta tags for the homepage

---
+++ resources/js/Components/Seo.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let title = "";
+    export let description = "";
+    export let keywords = "";
+    export let author = "";
+    export let ogTitle = title;
+    export let ogDescription = description;
+    export let ogImage = "";
+    export let ogUrl = "";
+    export let twitterCard = "summary_large_image";
+    // JSON-LD für strukturierte Daten
+    export let jsonLd = null;
+</script>
+
+<svelte:head>
+    <!-- Standard Meta-Tags -->
+    {#if title}<title>{title}</title>{/if}
+    {#if description}<meta name="description" content={description} />{/if}
+    {#if keywords}<meta name="keywords" content={keywords} />{/if}
+    {#if author}<meta name="author" content={author} />{/if}
+
+    <!-- Open Graph Meta-Tags -->
+    {#if ogTitle}<meta property="og:title" content={ogTitle} />{/if}
+    {#if ogDescription}<meta property="og:description" content={ogDescription} />{/if}
+    {#if ogImage}<meta property="og:image" content={ogImage} />{/if}
+    {#if ogUrl}<meta property="og:url" content={ogUrl} />{/if}
+    <meta property="og:type" content="website" />
+
+    <!-- Twitter Card Meta-Tags -->
+    {#if twitterCard}<meta name="twitter:card" content={twitterCard} />{/if}
+    {#if ogTitle}<meta name="twitter:title" content={ogTitle} />{/if}
+    {#if ogDescription}<meta name="twitter:description" content={ogDescription} />{/if}
+    {#if ogImage}<meta name="twitter:image" content={ogImage} />{/if}
+
+    <!-- JSON-LD für strukturierte Daten -->
+    {#if jsonLd}
+        {@html `<script type="application/ld+json">${JSON.stringify(jsonLd)}</script>`}
+    {/if}
+</svelte:head>
--- resources/js/Pages/Homepage.svelte
+++ resources/js/Pages/Homepage.svelte
@@ -2,8 +2,40 @@
     import AppLayout from "../Layout/AppLayout.svelte";
     import HeroSection from "../Components/HeroSection.svelte";
     import backgroundImage from "../../images/background.jpeg";
+    import Seo from "../Components/Seo.svelte";
 </script>
-
+<Seo
+    title="SV Polle"
+    description="Der SV Polle ist ein gemeinsamer Sportverein der Orte Andrup, Lage und Lotten. Besonders im Fußball und Dart aktiv."
+    keywords="SV Polle, Sportverein, Fußball, Dart, Haselünne"
+    author="SV Polle Team"
+    ogImage={backgroundImage}
+    ogUrl="https://www.sv-polle.de"
+    twitterCard="summary_large_image"
+    jsonLd={{
+        "@context": "https://schema.org",
+        "@type": "SportsOrganization",
+        "name": "SV Polle",
+        "legalName": "Sportverein Polle e. V.",
+        "sport": "Football, Darts",
+        "foundingDate": "1988",
+        "email": "info@sv-polle.de",
+        "url": "https://www.sv-polle.de",
+        "logo": "https://www.sv-polle.de/images/sv-polle-logo.png",
+        "sameAs": [
+            "https://www.instagram.com/sv_polle",
+            "https://www.fussball.de/verein/sv-polle-niedersachsen/-/id/00ES8GN80K000026VV0AG08LVUPGND5I#!/"
+        ],
+        "description": "Der SV Polle ist ein gemeinsamer Sportverein der Orte Andrup, Lage und Lotten. Besonders im Fußball und Dart aktiv.",
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Schützenstraße 8",
+            "addressLocality": "Haselünne",
+            "postalCode": "49740",
+            "addressCountry": "DE"
+        },
+    }}
+/>
 <AppLayout>
     <HeroSection title="Willkommen beim SV Polle" image={backgroundImage} />
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
