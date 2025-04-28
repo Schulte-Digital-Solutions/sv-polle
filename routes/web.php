553a3dac765801821fd6b5234e1ebf3ce7251718
Benedikt Schulte <benedikt@bullshitmedia.de>
2025-04-29 00:21:06 +0200

    Implement routing logic for 'coming soon' status in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomepageController;
 
-Route::get('/', [HomepageController::class, 'home'])->name('home');
+
+// Routing je nach Status
+if (env('APP_ENV', 'production') === 'coming_soon') {
+    Route::get('/', function () {
+        return view('coming-soon');
+    })->name('coming-soon');
+    // redirect all other routes to the coming soon page
+    Route::get('/{any}', function () {
+        return view('coming-soon');
+    })->where('any', '.*');
+} else {
+    Route::get('/', [HomepageController::class, 'home'])->name('home');
+}
